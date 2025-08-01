48f986f6ce5d8d7d80020d1299e534675a03abd1
Nilanjan Sasmal <nilanjan.physics137@gmail.com>
2025-08-01 22:09:54 +0530

    Update Comparaing Kitaev's Result.py

--- Comparaing Kitaev's Result.py
+++ Comparaing Kitaev's Result.py
@@ -2,8 +2,8 @@
 import numpy as np
 import scipy as sp
 import math
-from numpy.linalg import eigh, norm, svd       ##### To compute the Singular Value Decomposition
-from matplotlib import pyplot as plt           ##### Package required to plot
+from numpy.linalg import eigh, norm, svd                                    ##### To compute the Singular Value Decomposition
+from matplotlib import pyplot as plt                                        ##### Package required to plot
 import matplotlib as mpl
 from scipy import stats
 mpl.rcParams.update({"text.usetex":True})
@@ -25,18 +25,18 @@ b2 = (2 * np.pi / (3)) * np.array([np.sqrt(3), 1])
 ########### Function that returns the twisted boundary condition if given the input t, which is the amount of twist ###########
 ###############################################################################################################################
 
-def twist_ybc(L1, L2, t):                             ######## Twist for y-bonds
+def twist_ybc(L1, L2, t):                                                                            ######## Twist for y-bonds
     '''
-    ------------------------------------------------------------------------------------------------------
-    This function : twist_ybc(L1, L2, t) splits out the coordinates for the Twisted Boundary Conditions
-    with a finite twist in the y-bonds with magnitude "t";
-    ======================================================
-    Twist Parametr : t : datatype (int)
-    ======================================================
-    t = 0  : => PBC (Periodic Boundary Condition)
-    t = -1  : => APBC (Anti-Periodic Boundary Condition)
-    t >= 1  : => TBC (Twisted Boundary Condition)
-    ------------------------------------------------------------------------------------------------------
+    -------------------------------------------------------------------------------------------------------
+    This function : ``twist_ybc(L1, L2, t)`` splits out the coordinates for the Twisted Boundary Conditions
+    with a finite twist in the y-bonds with magnitude ``t`` for a Honeycomb Lattice of dimension L1 x L2;
+    =======================================================================================================
+    Twist Parametr : ``t`` : datatype (int)
+    =======================================================================================================
+    t = 0  : => PBC  (Periodic Boundary Condition)
+    t = -1 : => APBC (Anti-Periodic Boundary Condition) [Currently not available]
+    t >= 1 : => TBC  ((non-zero) Twisted Boundary Condition)
+    -------------------------------------------------------------------------------------------------------
     '''
     Ti = np.zeros(L1, dtype = int)
     Tf = np.zeros(L1, dtype = int)
@@ -46,7 +46,7 @@ def twist_ybc(L1, L2, t):                             ######## Twist for y-bonds
         Ti[y] = y * L2
     for y in range(1, L1 + 1):
         Tf[y-1] = L1 * L2 + (y * L2) - 1
-    # Twisted and Periodic Boundary Condition
+    #### Twisted and Periodic Boundary Conditions ####
     for y in range(0, L1):
         tb[y, 0] = Ti[y]
         tb[y, 1] = Tf[(y + t) % L1]
@@ -58,8 +58,8 @@ def twist_ybc(L1, L2, t):                             ######## Twist for y-bonds
 
 def Ham_0(L1, L2, t, jx, jy, jz):
     m = L1 * L2                      ################################################## Total number of unit cells in the lattice
-    N = m * 2                        ## Total number of sites, including the sublattice points (each unit cell includes 2 sitess)
-    H = np.zeros((N, N))             ####################### Defining the Null Matrix Beforehand as most of the entries will be 0
+    N = m * 2                        ### Total number of sites, including the sublattice points (each unit cell includes 2 sites)
+    H = np.zeros((N, N))             ###################### Defining the Null Matrix Beforehand, as most of the entries will be 0
     ty = twist_ybc(L1, L2, t)        ############################## Importing the boundary conditions with a twist in y-direction
     
 ######################## Defining the Matrix elements according to the gauge choices for different links ########################
@@ -77,7 +77,7 @@ def Ham_0(L1, L2, t, jx, jy, jz):
         H[k+m-L2, k] = -jz
     
 ##################################  Applying the PBC (Periodic Boundary Conditions)  ############################################
-    for b in range(0, L2):           ###### For Vertical bonds (zz-links) no twist in z-direction for the moment
+    for b in range(0, L2):           ###### For Vertical bonds (zz-links), no twist in z-direction for the moment
         H[b, N-L2+b] = +jz
         H[N-L2+b, b] = -jz
 
@@ -98,24 +98,24 @@ def E_0(L1, L2, t, jx, jy, jz):
     ##### Determine Nu
     Nu = H0.shape[0] // 2
     ##### Split the matrix into four N x N blocks
-    F = H0[:Nu, :Nu]                                            # Top-left block
-    M = H0[:Nu, Nu:]                                            # Top-right block
-    MT = (-1) * H0[Nu:, :Nu]                                    # Bottom-left block
-    D = (-1) * H0[Nu:, Nu:]                                     # Bottom-right block
-    u, s, vT = sp.linalg.svd(M, full_matrices = False)          # SVD of M
+    F = H0[:Nu, :Nu]                                            #### Top-left block
+    M = H0[:Nu, Nu:]                                            #### Top-right block
+    MT = (-1) * H0[Nu:, :Nu]                                    #### Bottom-left block
+    D = (-1) * H0[Nu:, Nu:]                                     #### Bottom-right block
+    u, s, vT = sp.linalg.svd(M, full_matrices = False)          #### SVD of M
     # e, p = sp.linalg.eigh(M)
     
-    return s                                                    # return only the singular values
+    return s                                                    #### return only the singular values
 
 
 ##########################################################################################################################################
 ############################################## Real Space Hamiltonian for 2 flux state ###################################################
 ##########################################################################################################################################
-# mu = 0                                      # Spin Flip at mu-th unit cell (z-type unit cell)! : mu = 0 implies, (4,16) for 4*4 lattice! 
+# mu = 0                                      # Spin Flip at mu-th unit cell (z-type unit cell)!: mu = 0 implies (4,16) for 4 x 4 lattice! 
 def Ham_2(L1, L2, t, jx, jy, jz, mu):
     m = L1 * L2                               # Total number of unit cells in the lattice
-    N = m * 2                                 # Total number of sites, including the sublattice points (each unit cell includes 2 sitess)
-    H = np.zeros((N, N))                      # Defining the Null Matrix Beforehand as most of the entries will be 0
+    N = m * 2                                 # Total number of sites, including the sublattice points (each unit cell includes 2 sites)
+    H = np.zeros((N, N))                      # Defining the Null Matrix Beforehand, as most of the entries will be 0
     ty = twist_ybc(L1, L2, t)                 # Importing the boundary conditions with a twist in y-direction
     
 ######### Defining the Matrix elements according to the gauge choices for different links
@@ -133,7 +133,7 @@ def Ham_2(L1, L2, t, jx, jy, jz, mu):
         H[k+m-L2, k] = -jz
     
 ###### Applying the PBC (Periodic Boundary Conditions)
-    for b in range(0, L2):                    # For Vertical bonds (zz-links) no twist in z-direction for the moment
+    for b in range(0, L2):                    # For Vertical bonds (zz-links), no twist in z-direction for the moment
         H[b, N-L2+b] = +jz
         H[N-L2+b, b] = -jz
 
@@ -141,7 +141,7 @@ def Ham_2(L1, L2, t, jx, jy, jz, mu):
         H[ty[y][0], ty[y][1]] = +jy
         H[ty[y][1], ty[y][0]] = -jy
 
-############ Introducing the Single Spin Filp terms in z-bonds! (in the bulk, Boundary Conditions also included!)
+############ Introducing the Single Spin Flip terms in z-bonds! (In the bulk, Boundary Conditions also included!)
     if (mu < m-L2):                           # For bulk z-spin-flip
         H[L2+mu, m+mu] = -jz
         H[m+mu, L2+mu] = +jz
@@ -159,14 +159,14 @@ def Ham_2(L1, L2, t, jx, jy, jz, mu):
 def E_2f(L1, L2, t, jx, jy, jz, mu):
     N = L1 * L2 * 2 
     H2 = Ham_2(L1, L2, t, jx, jy, jz, mu)        
-    # Determine Nu
+    #### Determine Nu
     Nu = H2.shape[0] // 2
-    # Split the matrix into four N x N blocks
-    F = H2[:Nu, :Nu]                                               # Top-left block
-    M = H2[:Nu, Nu:]                                               # Top-right block
-    MT = (-1) * H2[Nu:, :Nu]                                       # Bottom-left block
-    D = (-1) * H2[Nu:, Nu:]                                        # Bottom-right block
-    u2, s2, v2T = sp.linalg.svd(M, full_matrices = False)          # SVD of M
+    #### Split the matrix into four (N x N) blocks
+    F = H2[:Nu, :Nu]                                               #### Top-left block
+    M = H2[:Nu, Nu:]                                               #### Top-right block
+    MT = (-1) * H2[Nu:, :Nu]                                       #### Bottom-left block
+    D = (-1) * H2[Nu:, Nu:]                                        #### Bottom-right block
+    u2, s2, v2T = sp.linalg.svd(M, full_matrices = False)          #### SVD of M
     
     return s2
 
@@ -221,11 +221,11 @@ rE0t = np.array(rE0t)
 ################# Ground State Energy per unit cell in the 2-flux state (Calculations for Periodic Boundary Conditions) ##################
 ##########################################################################################################################################
 
-GE2p = []                                          ### Empty List to store the Ground State Energy per unit cell of a 2-flux state in PBC
-L2p = 20                                           ### Maximum System Size
-jx = jy = jz = 1                                   ### Model Parameters (Ising interaction Strengths)
-t2p = 0                                            ### Twist Parameter (= 0 implies PBC)
-mu2p = 4                                           ### mu2p is the unit cell number (which gives a coordinate) of a single spin flip
+GE2p = []                                           ### Empty List to store the Ground State Energy per unit cell of a 2-flux state in PBC
+L2p = 20                                            ### Maximum System Size
+jx = jy = jz = 1                                    ### Model Parameters (Ising interaction Strengths)
+t2p = 0                                             ### Twist Parameter (= 0 implies PBC)
+mu2p = 4                                            ### mu2p is the unit cell number (which gives a coordinate) of a single spin flip
 sys_size2p = np.linspace(3, L2p, L2p-2).astype(int)
 for l in sys_size2p:
     sing_2 = E_2f(l, l, t2p, jx, jy, jz, mu2p)       ### Extracting the singular values
@@ -245,11 +245,11 @@ rE2p = np.array(rE2p)
 ################# Ground State Energy per unit cell in the 2-flux state (Calculations for Twisted Boundary Conditions) ###################
 ##########################################################################################################################################
 
-GE2t = []                                          ### Empty List to store the Ground State Energy per unit cell of a 2-flux state in PBC
-L2t = 20                                           ### Maximum System Size
-jx = jy = jz = 1                                   ### Model Parameters (Ising interaction Strengths)
-t2t = 0                                            ### Twist Parameter (= 1 implies TBC)
-mu2t = 4                                           ### mu2t is the unit cell number (which gives a coordinate) of a single spin flip
+GE2t = []                                           ### Empty List to store the Ground State Energy per unit cell of a 2-flux state in PBC
+L2t = 20                                            ### Maximum System Size
+jx = jy = jz = 1                                    ### Model Parameters (Ising interaction Strengths)
+t2t = 0                                             ### Twist Parameter (= 1 implies TBC)
+mu2t = 4                                            ### mu2t is the unit cell number (which gives a coordinate) of a single spin flip
 sys_size2t = np.linspace(3, L2t, L2t-2).astype(int)
 for l in sys_size2t:
     sing_2 = E_2f(l, l, t2t, jx, jy, jz, mu2t)       ### Extracting the singular values
@@ -279,7 +279,7 @@ plt.grid(True, linestyle = "--", alpha = 0.5)
 plt.axhline(y = -1.5745974, color='g', linestyle='--', linewidth = 0.5, label=r"$E_{GS}^0 \approx -1.5745974$")
 plt.legend()
 plt.tight_layout()
-# plt.savefig("0-flux_GS_energy_perunitcell_vs_systemsize.pdf", bbox_inches="tight")
+# plt.savefig("0-flux_GS_energy_perunitcell_vs_systemsize.pdf", bbox_inches="tight")       #### To save to plot as .pdf (format)
 plt.show()
 
-############ Similarly You Can Plot Everything and see and compare the Results to that what Kitaev have given in page: 45 of his paper ##
+############ Similarly, You Can Plot Everything and see and compare the Results to what Kitaev has given on page 45 of his paper #########
